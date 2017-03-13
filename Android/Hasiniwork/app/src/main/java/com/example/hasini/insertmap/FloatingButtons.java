56fcbfee8e0b22ac09d67eb5602b6e21eed1f01d
HasiniErandi <hasinierandi810@gmail.com>
2017-03-13 07:01:19 -0700

    Hasini Commited

---
+++ Android/Hasiniwork/app/src/main/java/com/example/hasini/insertmap/FloatingButtons.java
@@ -0,0 +1,15 @@
+package com.example.hasini.insertmap;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hasini on 3/10/2017.
+ */
+
+public class FloatingButtons extends AppCompatActivity {
+    String detect;
+
+
+
+
+}
